fbcf785f1a96c321c7e33a7afb0830d5208d9cc4
namahin <mahin0w1@gail.com>
2021-01-01 21:54:38 +0600

    update info

--- js/common.js
+++ js/common.js
@@ -86,7 +86,7 @@ $(document).ready(function () {
             sliderWork = work.find('.js-work-slider');
 
         sliderWork.slick({
-            slidesToShow: 4,
+            slidesToShow: 3,
             slidesToScroll: 1,
             dots: false,
             arrows: true,

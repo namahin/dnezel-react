3d821124201515fea659c36bcad7f27cbf9c321c
namahin <mahin0w1@gail.com>
2021-01-15 18:24:21 +0600

    update all items

--- js/common.js
+++ js/common.js
@@ -204,3 +204,23 @@ AOS.init();
         return false;
     });
 })();
+
+
+// testimonial slider
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+})
